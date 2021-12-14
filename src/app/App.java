3d48f02b66fdf60109c49de529d00be36db31a29
Marcelo Campos <marcelo.ildefonso@hotmail.com>
2021-12-14 08:26:41 -0300

    project update

--- src/app/App.java
+++ src/app/App.java
@@ -1,3 +1,4 @@
+package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá Mundo com java-vscode by marceloicampos with javaSE-17");
